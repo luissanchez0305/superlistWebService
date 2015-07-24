38a5f2a791786e65ec58275c43cca134ba0ce1cc
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2015-07-24 13:46:00 -0500

    productos

--- controllers/productos.php
+++ controllers/productos.php
@@ -71,8 +71,8 @@ else if($_GET['type'] == 'compra'){
 
 function loadProducto($producto, $marca, $producto_lugar){
 	$productoModel = new producto();
-	echo $producto->nombre.'-';
 	$productoModel->nombre = $producto->nombre;
+	echo '-'.$productoModel->nombre.'-';
 	$productoModel->id = $producto->id;
 	$productoModel->imagen = !is_null($producto->imagen) ? '/uploads/users/' . $producto->imagen : '/uploads/default.jpg';
 	$productoModel->marcaId = $marca->id;
